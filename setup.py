5d339d99a1b9588765e2ac857052e1bcdecd8848
Andrew Ridden-Harper <andrew.ridden-harper@canterbury.ac.nz>
2024-07-04 15:45:26 +1200

    test

--- setup.py
+++ setup.py
@@ -8,5 +8,7 @@ setup(
     description="Code for GM prediction mixed-effects regression analysis",
     install_requires=[
         "pymer4 @ git+https://github.com/ucgmsim/pymer4.git@get_ranef_cond_std",
+        "numpy==1.26.4",
+        "natsort",
     ],
 )

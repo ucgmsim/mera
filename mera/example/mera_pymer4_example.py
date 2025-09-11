27fa5169905e70f3bd2384561766f9620d57e32b
Claudio <claudio.schill@canterbury.ac.nz>
2025-09-12 09:25:21 +1200

    Tidy up exampled

--- mera/example/mera_pymer4_example.py
+++ mera/example/mera_pymer4_example.py
@@ -4,81 +4,82 @@ from pathlib import Path
 
 import mera
 
-# Load the data
-data_dir = Path(__file__).parent / "resources"
-output_dir = Path(__file__).parent / "residuals"
+if __name__ == "__main__":
+    # Load the data
+    data_dir = Path(__file__).parent / "resources"
+    output_dir = Path(__file__).parent / "residuals"
 
-output_dir.mkdir(exist_ok=True)
+    output_dir.mkdir(exist_ok=True)
 
-obs_ffp = data_dir / "im_obs.csv"
-sim_ffp = data_dir / "im_sim.csv"
+    obs_ffp = data_dir / "im_obs.csv"
+    sim_ffp = data_dir / "im_sim.csv"
 
-obs_df = pd.read_csv(obs_ffp, index_col=0)
-sim_df = pd.read_csv(sim_ffp, index_col=0)
+    obs_df = pd.read_csv(obs_ffp, index_col=0)
+    sim_df = pd.read_csv(sim_ffp, index_col=0)
 
-# Sanity checking
-# Ensure the index matches
-assert (
-    np.all(obs_df.index == sim_df.index)
-    and np.all(obs_df.event_id == sim_df.event_id)
-    and np.all(obs_df.stat_id == sim_df.stat_id)
-)
+    # Sanity checking
+    # Ensure the index matches
+    assert (
+        np.all(obs_df.index == sim_df.index)
+        and np.all(obs_df.event_id == sim_df.event_id)
+        and np.all(obs_df.stat_id == sim_df.stat_id)
+    )
 
-# List of IMs of interest
-# Just using all shared pSA periods in the data files
-ims = [
-    cur_im
-    for cur_im in np.intersect1d(obs_df.columns, sim_df.columns)
-    if cur_im.startswith("pSA")
-    or cur_im in ["PGA", "CAV", "Ds575", "Ds595", "AI", "PGV"]
-]
+    # List of IMs of interest
+    # Just using all shared pSA periods in the data files
+    ims = [
+        cur_im
+        for cur_im in np.intersect1d(obs_df.columns, sim_df.columns)
+        if cur_im.startswith("pSA")
+        or cur_im in ["PGA", "CAV", "Ds575", "Ds595", "AI", "PGV"]
+    ]
 
-# Compute the residual
-res_df = np.log(obs_df[ims] / sim_df[ims])
+    # Compute the residual
+    res_df = np.log(obs_df[ims] / sim_df[ims])
 
-# Add event id and station id columns
-res_df["event_id"] = obs_df.event_id
-res_df["stat_id"] = obs_df.stat_id
+    # Add event id and station id columns
+    res_df["event_id"] = obs_df.event_id
+    res_df["stat_id"] = obs_df.stat_id
 
-# Ensure that the event and site ids are different (i.e. no common values)
-res_df["event_id"] = np.char.add("event_", res_df["event_id"].values.astype(str))
-res_df["stat_id"] = np.char.add("stat_", res_df["stat_id"].values.astype(str))
+    # Ensure that the event and site ids are different (i.e. no common values)
+    res_df["event_id"] = np.char.add("event_", res_df["event_id"].values.astype(str))
+    res_df["stat_id"] = np.char.add("stat_", res_df["stat_id"].values.astype(str))
 
-# Optional: Define an initial mask.
-# A random mask is generated for this example.
-# The fraction of True and False can be changed with the p = [] parameter.
-mask = pd.DataFrame(
-    np.random.choice([True, False], p=[1.0, 0.0], size=res_df[ims].shape),
-    columns=res_df[ims].columns,
-    index=res_df[ims].index,
-)
+    # Optional: Define an initial mask.
+    # A random mask is generated for this example.
+    # The fraction of True and False can be changed with the p = [] parameter.
+    mask = pd.DataFrame(
+        np.random.choice([True, False], p=[1.0, 0.0], size=res_df[ims].shape),
+        columns=res_df[ims].columns,
+        index=res_df[ims].index,
+    )
 
-# Optional: Mask out records without sufficient records per station/event.
-mask = mera.mask_too_few_records(
-    res_df,
-    mask=mask,
-    event_cname="event_id",
-    site_cname="stat_id",
-    min_num_records_per_event=3,
-    min_num_records_per_site=3,
-)
+    # Optional: Mask out records without sufficient records per station/event.
+    mask = mera.mask_too_few_records(
+        res_df,
+        mask=mask,
+        event_cname="event_id",
+        site_cname="stat_id",
+        min_num_records_per_event=3,
+        min_num_records_per_site=3,
+    )
 
-# Optional: compute site term (recommended)
-compute_site_term = True
+    # Optional: compute site term (recommended)
+    compute_site_term = True
 
-# # Run MER
-results = mera.run_mera(
-    res_df,
-    list(ims),
-    "event_id",
-    "stat_id",
-    compute_site_term=compute_site_term,
-    mask=mask,
-    verbose=True,
-    raise_warnings=True,
-    min_num_records_per_event=3,
-    min_num_records_per_site=3,
-)
+    # # Run MER
+    results = mera.run_mera(
+        res_df,
+        list(ims),
+        "event_id",
+        "stat_id",
+        compute_site_term=compute_site_term,
+        mask=mask,
+        verbose=True,
+        raise_warnings=True,
+        min_num_records_per_event=3,
+        min_num_records_per_site=3,
+    )
 
-# Save the results
-results.save(output_dir)
+    # Save the results
+    results.save(output_dir)
